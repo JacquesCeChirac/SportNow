3c93b4a6ca0b205fd35e0712b4f89cead391e3a7
JacquesCeChirac <nicolas.cherridi@gmail.com>
2018-04-17 16:26:34 +0200

    base

---
+++ backendsportnow/src/main/java/wtf/nicoto/backendsportnow/BackendsportnowApplication.java
@@ -0,0 +1,12 @@
+package wtf.nicoto.backendsportnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsportnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsportnowApplication.class, args);
+	}
+}
